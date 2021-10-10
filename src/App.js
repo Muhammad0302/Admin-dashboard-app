c05cde25a97820eacbe627da08cd9b169a68fce0
Lucky Boy <mi477048@gmail.com>
2021-10-10 10:24:12 -0700

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-
+import Home from "./components/home/Home";
 function App() {
   return (
-    <div className="App">
+    <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./home.css";
+function Home() {
+  return <div className="home">others</div>;
+}
+
+export default Home;
